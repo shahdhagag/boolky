03ba6079b7a9373ebe0de38e86c024a352040dda
dell <shahdhagag546@gmail.com>
2025-12-14 20:06:00 +0200

    create splach view body

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:bookly/features/spalsh/presentation/views/splash_view.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const Bookly());
+}
+
+class Bookly extends StatelessWidget {
+  const Bookly({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: const SplashView(),
+    );
+  }
+}
