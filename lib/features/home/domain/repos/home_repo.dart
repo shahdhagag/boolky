82d3c18642893fb042dc9775f3ed148194863af6
dell <shahdhagag546@gmail.com>
2025-12-16 20:42:16 +0200

    add dartz package ans Either class

--- lib/features/home/domain/repos/home_repo.dart
+++ lib/features/home/domain/repos/home_repo.dart
@@ -1,7 +1,10 @@
 import 'package:bookly/features/home/domain/entities/book_entity.dart';
+import 'package:dartz/dartz.dart';
+
+import '../../../../core/errors/failure.dart';
 
 abstract class HomeRepo {
-  Future<List<BookEntity>> fetchFeaturedBooks();
+  Future<Either<Failure, List<BookEntity>>> fetchFeaturedBooks();
 
-  Future<List<BookEntity>> fetchNewestBooks();
+  Future<Either<Failure, List<BookEntity>>> fetchNewestBooks();
 }
