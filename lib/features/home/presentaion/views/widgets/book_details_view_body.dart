7f72fb57addbaef3a8a13826f808e4715915592e
dell <shahdhagag546@gmail.com>
2025-12-16 16:11:21 +0200

    add title and subtitle to details screen

--- lib/features/home/presentaion/views/widgets/book_details_view_body.dart
+++ lib/features/home/presentaion/views/widgets/book_details_view_body.dart
@@ -1,4 +1,6 @@
+import 'package:bookly/core/utiles/styles.dart';
 import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
 
 import 'custom_book_details_appbar.dart';
 import 'custom_book_image.dart';
@@ -15,8 +17,21 @@ class BookDetailsViewBody extends StatelessWidget {
         children: [
           const CustomBookDetailsAppBare(),
           Padding(
-            padding: EdgeInsets.symmetric(horizontal: width * .18),
+            padding: EdgeInsets.symmetric(horizontal: width * .2),
             child: const CustomBookImage(),
+          ),
+          Gap(43),
+          Text(
+            "The Jungle Book",
+            style: Styles.textStyle30.copyWith(fontWeight: FontWeight.bold),
+          ),
+          Gap(6),
+          Opacity(
+            opacity: .7,
+            child: Text(
+              "Rudyard Kipling",
+              style: Styles.textStyle18.copyWith(fontStyle: FontStyle.italic),
+            ),
           ),
         ],
       ),
