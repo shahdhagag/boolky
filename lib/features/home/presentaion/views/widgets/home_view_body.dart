da6f5deba223bb85a3139780852084dd58bb3623
dell <shahdhagag546@gmail.com>
2025-12-16 14:56:45 +0200

    best seller list view done

--- lib/features/home/presentaion/views/widgets/home_view_body.dart
+++ lib/features/home/presentaion/views/widgets/home_view_body.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:gap/gap.dart';
 
 import '../../../../../core/utiles/styles.dart';
-import 'best_seller_list_view_item.dart';
+import 'best_seller_list_view.dart';
 import 'custom_appbar.dart';
 import 'featured_list_view_body.dart';
 
@@ -11,19 +11,35 @@ class HomeViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 30.0),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          const CustomAppBar(),
-          const FeaturedBookListView(),
-          const Gap(40),
-          Text("Best Seller", style: Styles.textStyle18),
-          const Gap(20),
-          BestSellerListViewItem(),
-        ],
-      ),
+    return CustomScrollView(
+      slivers: [
+        SliverToBoxAdapter(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 30.0),
+                child: const CustomAppBar(),
+              ),
+              const FeaturedBookListView(),
+              const Gap(40),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 30.0),
+                child: Text("Best Seller", style: Styles.textStyle18),
+              ),
+              const Gap(20),
+              // BestSellerListView(),
+            ],
+          ),
+        ),
+        const SliverFillRemaining(
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 30.0),
+            child: BestSellerListView(),
+          ),
+        )
+      ],
+
     );
   }
 }
