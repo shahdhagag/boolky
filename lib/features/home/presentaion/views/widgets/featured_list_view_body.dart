45138feec941c82218573f7e9c6660f1e9312f1b
dell <shahdhagag546@gmail.com>
2025-12-16 15:58:42 +0200

    add custom book image to the book details screen

--- lib/features/home/presentaion/views/widgets/featured_list_view_body.dart
+++ lib/features/home/presentaion/views/widgets/featured_list_view_body.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-import 'custom_list_view_item.dart';
+import 'custom_book_image.dart';
 
 class FeaturedBookListView extends StatelessWidget {
   const FeaturedBookListView({super.key});
@@ -14,7 +14,7 @@ class FeaturedBookListView extends StatelessWidget {
         itemBuilder: (context, index) {
           return const Padding(
             padding: EdgeInsets.symmetric(horizontal: 8.0),
-            child: FeaturedListViewItem(),
+            child: CustomBookImage(),
           );
         },
       ),
